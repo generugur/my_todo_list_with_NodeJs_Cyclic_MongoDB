b4b9e8b93d3d9f178eb55143edc9c983e7ba84a1
generugur <107924646+generugur@users.noreply.github.com>
2023-03-12 14:32:40 +0300

    added bootstrap 5.2 cdn links

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ const listSchema = {
 const List = mongoose.model("List", listSchema);
 
 
+
+
 app.get("/", function(req, res) {
   Item.find({}, function(err, foundItems) {
     if (foundItems.length === 0) {
